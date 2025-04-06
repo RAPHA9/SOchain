522492573353d03b8c4a4622bb59d01edb159fbe
Rapha <146448338+RAPHA9@users.noreply.github.com>
2025-04-06 01:17:44 +0100

    Update process.c

--- SOCHAIN/src/process.c
+++ SOCHAIN/src/process.c
@@ -1,54 +1,59 @@
 #include "process.h"
-#include <stdio.h>
+#include "wallet.h"   // Adicionado para declarar execute_wallet
+#include "server.h"   // Adicionado para declarar execute_server
 #include <stdlib.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
-
-/* Função que inicia um novo processo Wallet */
+/* Função que inicia um novo processo Wallet através do fork.
+ * No processo filho, executa execute_wallet e encerra com o retorno da função.
+ * No processo pai, retorna o pid do processo criado.
+ */
 int launch_wallet(int wallet_id, struct info_container* info, struct buffers* buffs) {
-   pid_t pid = fork();
-
-   if(pid < 0){
-        perror("Error creating wallet");
-        return -1;
-   }
-   else if(pid == 0){
-        exit(execute_wallet(wallet_id, info, buffs));    
-   }
-
-   return pid;
+    pid_t pid = fork();
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0) {
+        int ret = execute_wallet(wallet_id, info, buffs);
+        exit(ret);
+    }
+    return (int)pid;
 }
 
-/* Função que inicia um novo processo Server */
+/* Função que inicia um novo processo Server através do fork.
+ * No processo filho, executa execute_server e encerra com o retorno da função.
+ * No processo pai, retorna o pid do processo criado.
+ */
 int launch_server(int server_id, struct info_container* info, struct buffers* buffs) {
     pid_t pid = fork();
-
-    if (pid < 0) {  
-        perror("Erro ao criar processo Server");
-        return -1;
-    } 
-    else if (pid == 0) {  
-        exit(execute_server(server_id, info, buffs)); 
+    if (pid < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
-
-    return pid;  
+    if (pid == 0) {
+        int ret = execute_server(server_id, info, buffs);
+        exit(ret);
+    }
+    return (int)pid;
 }
 
-/* Função que espera que um processo com PID process_id termine */
+/* Função que espera que o processo com PID process_id termine.
+ * Retorna o valor de saída do processo se este terminar normalmente.
+ */
 int wait_process(int process_id) {
     int status;
-    pid_t result = waitpid(process_id, &status, 0);  
-
-    if (result == -1) {  
-        perror("Erro ao esperar pelo processo");
+    pid_t w = waitpid(process_id, &status, 0);
+    if (w == -1) {
+        perror("waitpid");
+        exit(EXIT_FAILURE);
+    }
+    if (WIFEXITED(status)) {
+        return WEXITSTATUS(status);
+    } else {
         return -1;
     }
-
-    if (WIFEXITED(status)) { 
-        return WEXITSTATUS(status); 
-    }
-
-    return -1;  
 }
